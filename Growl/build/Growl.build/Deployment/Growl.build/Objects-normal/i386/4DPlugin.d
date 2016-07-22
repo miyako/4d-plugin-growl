97ffbe4513bf0c6b2f95feadb772ad158d379576
Keisuke Miyako <miyako@wakanda.jp>
2016-07-22 18:34:58 +0900

    added constants, cleanup framework

---
+++ Growl/build/Growl.build/Deployment/Growl.build/Objects-normal/i386/4DPlugin.d
@@ -0,0 +1,25 @@
+dependencies: \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4DPlugin.cpp \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/4DPluginAPI.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Flags.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/PublicTypes.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_INTEGER.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_LONGINT.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_TIME.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_DATE.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_REAL.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_TEXT.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_BLOB.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_POINTER.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/C_PICTURE.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_TEXT.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_BOOLEAN.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_INTEGER.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_REAL.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_LONGINT.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_TIME.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4D\ Plugin\ API/Classes/ARRAY_DATE.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/4DPlugin.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/Frameworks/Growl.framework/Versions/A/Headers/Growl.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/Frameworks/Growl.framework/Versions/A/Headers/GrowlDefines.h \
+  /Users/miyako/Documents/GitHub/4d-plugin/4d-plugin-growl/Growl/Frameworks/Growl.framework/Versions/A/Headers/GrowlApplicationBridge.h
